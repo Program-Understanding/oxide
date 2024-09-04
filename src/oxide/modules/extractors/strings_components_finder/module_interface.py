a1ba6034862db5295baf74a62594abe85fd52960
GraysonSparks <g77sparks@gmail.com>
2024-09-04 00:37:50 -0500

    strings_components_finder changes for firmline

--- src/oxide/modules/extractors/strings_components_finder/module_interface.py
+++ src/oxide/modules/extractors/strings_components_finder/module_interface.py
@@ -5,6 +5,8 @@ MUST "pip install pyahocorasick" FOR THIS MODULE TO FUNCTION PROPERLY
 
 If testing for possible components, it is common for the terminal space fill up; all solutions may not be visible.
 
+Must run this module with a component option. 
+"--component=architecture" , "--component=core" , "--component=device" 
 Possible components: "run strings_components_finder --mode=possible &YOUR_COLLECTION | show"
 Explicit components only: "run strings_components_finder &YOUR_COLLECTION | show"
 
@@ -78,111 +80,91 @@ def process(oid, opts):
 
     logger.debug("process(strings)")
 
-    strings_retrieved = api.retrieve("strings", oid)[oid] #This api call retrieves the strings of the test file using the strings analyzer module.
+    strings_retrieved = api.retrieve("strings", oid)[oid]
     strings_list = [value.upper() for value in strings_retrieved.values()]
     strings = set(strings_list)
     if len(strings) == 0: return False
 
 
     explicitly_found_dict = {}
-    possibly_found_dict = {}
+    components_found_dict = {}
+    components_found_dict = {}
     components_found_dict = {}
     processed_components = {}
-    processed_architectures = set()
-    processed_cores = set()
-    processed_devices = set()
+    processed_archs = {}
+    processed_cores = {}
+    processed_devices = {}
     
     processed_components = search_components_in_strings(strings, all_components)
+    
+    explicitly_found_dict["archs"] = {}
+    explicitly_found_dict["cores"] = {}
+    explicitly_found_dict["devices"] = {}
 
     if "mode" in opts:
         mode = str(opts["mode"])
     
     if "component" in opts:
         component = str(opts["component"])
+    
+    if mode == "explicit":
+        for string, occurrence in processed_components.items():
+            if string in arch_to_cores_data and component == "architecture":
+                components_found_dict[string] = occurrence
 
-    if mode == "possible":
-    #This loop checks if any of the file's strings are found in the components dataset.
-        if component == "none":
-            for string, occurence in processed_components.items(): 
-                occurence = str(occurence)
-                if string in arch_to_cores_data:
-                    explicitly_found_dict[string] = "instruction set architecture explicitly found " + occurence + " time/s"
-                    processed_architectures.add(string)
-                elif string in cores_to_IC_data:
-                    explicitly_found_dict[string] = "core explicitly found " + occurence + " time/s"
-                    processed_cores.add(string)
-                elif string in IC_devices_data:
-                    explicitly_found_dict[string] = "device explicitly found " + occurence + " time/s"
-                    processed_devices.add(string)
+            if string in cores_to_IC_data and component == "core":
+                components_found_dict[string] = occurrence
 
-    #These loops detect the possible components of the binary file. They do this by using the explicitly found components and retrieving components related to it from the dataset.
-        for IC_device in processed_devices:  
-            try: architecture = IC_to_arch_data[IC_device]
-            except: continue
-            if architecture not in explicitly_found_dict and architecture not in possibly_found_dict:
-                possibly_found_dict[architecture] = "instruction set architecture possible  ∵  " + IC_device + " device was found"
-
-            try: core = IC_to_cores_data[IC_device]
-            except: continue
-            if core not in explicitly_found_dict and core not in possibly_found_dict: 
-                possibly_found_dict[core] = "core possible  ∵  " + IC_device + " device was found"
-
-        for core in processed_cores:
-            try: architecture = cores_to_arch_data[core]
-            except: continue
-            if architecture not in explicitly_found_dict and architecture not in possibly_found_dict:
-                possibly_found_dict[architecture] = "instruction set architecture possible  ∵  " + core + " core was found"
-
-            try: IC_device_list = cores_to_IC_data[core]
-            except: continue
-            for IC_device in IC_device_list:
-                if IC_device not in explicitly_found_dict and IC_device not in possibly_found_dict:
-                    possibly_found_dict[IC_device] = "device possible  ∵  " + core + " core was found"
-
-        for architecture in processed_architectures:
-            try: core_list = arch_to_cores_data[architecture]
-            except: continue
-            for core in core_list:
-                if core not in explicitly_found_dict and core not in possibly_found_dict:
-                    possibly_found_dict[core] = "core possible  ∵  " + architecture + " instruction set architecture was found"
-
-            try: IC_device_list = arch_to_IC_data[architecture]
-            except: continue
-            for IC_device in IC_device_list:
-                if IC_device not in explicitly_found_dict and IC_device not in possibly_found_dict:
-                    possibly_found_dict[IC_device] = "device possible  ∵  " + architecture + " instruction set architecture was found"
-            components_found_dict = {**possibly_found_dict}             
-    elif mode == "explicit":
-        if component == "none":
-            for string, occurence in processed_components.items(): 
-                occurence = str(occurence)
-                if string in arch_to_cores_data:
-                    explicitly_found_dict[string] = "instruction set architecture explicitly found " + occurence + " time/s"
-                    processed_architectures.add(string)
-                elif string in cores_to_IC_data:
-                    explicitly_found_dict[string] = "core explicitly found " + occurence + " time/s"
-                    processed_cores.add(string)
-                elif string in IC_devices_data:
-                    explicitly_found_dict[string] = "device explicitly found " + occurence + " time/s"
-                    processed_devices.add(string)
-            components_found_dict = {**explicitly_found_dict}
-        else:
-            for string, occurence in processed_components.items(): 
-                occurence = str(occurence)
-                if (component == "architecture") and (string in arch_to_cores_data):
-                    explicitly_found_dict[string] = "instruction set architecture explicitly found " + occurence + " time/s"
-                elif (component == "core") and (string in cores_to_IC_data):
-                    explicitly_found_dict[string] = "core explicitly found " + occurence + " time/s"
-                elif (component == "device") and (string in IC_devices_data):
-                    explicitly_found_dict[string] = "device explicitly found " + occurence + " time/s"
-            components_found_dict = {**explicitly_found_dict}
+            if string in IC_devices_data and component == "device":
+                components_found_dict[string] = occurrence
 
 
- 
+    elif mode == "possible":
+        for string, occurrence in processed_components.items():
+            occurrence = 0
+            if string in arch_to_cores_data:
+                processed_archs[string] = ""
+
+            if string in cores_to_IC_data:
+                processed_cores[string] = ""
+
+            if string in IC_devices_data:
+                processed_devices[string] = ""
+
+        if component == "architecture":
+            for core in processed_cores:
+                if core in cores_to_arch_data:
+                    components_found_dict[cores_to_arch_data[core]] = ""
+            for device in processed_devices:
+                if device in IC_to_arch_data:
+                    components_found_dict[IC_to_arch_data[device]] = ""
+
+        elif component == "device":
+            for arch in processed_archs:
+                if arch in arch_to_IC_data:
+                    for value in arch_to_IC_data[arch]:
+                        components_found_dict[value] = ""
+            for core in processed_cores:
+                if core in cores_to_IC_data:
+                    for value in cores_to_IC_data[core]:
+                        components_found_dict[value] = ""
+
+        elif component == "core":
+            for arch in processed_archs:
+                if arch in arch_to_cores_data:
+                    for value in arch_to_cores_data[arch]:
+                        if value != "":
+                            components_found_dict[value] = ""
+            for device in processed_devices:
+                if device in IC_to_cores_data:
+                    for value in IC_to_cores_data[device]:
+                        components_found_dict[value] = ""
+        
+
 
     if len(components_found_dict.keys()) == 0:
         logger.debug("No strings found")
 
     if components_found_dict is None: return False
-    api.store(NAME, oid, components_found_dict, opts) #This api call stores our results for the cache
+    api.store(NAME, oid, components_found_dict, opts)
     return True
