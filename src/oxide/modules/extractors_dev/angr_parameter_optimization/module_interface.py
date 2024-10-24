eeda0f5def822bb145ebc7b9e6ef11abaf894dd9
bakerk98 <baker.kevan98@gmail.com>
2024-10-24 17:39:13 -0500

    updated angr parameter optimization to run w/ and w/o different tactics/exploration options

--- src/oxide/modules/extractors_dev/angr_parameter_optimization/module_interface.py
+++ src/oxide/modules/extractors_dev/angr_parameter_optimization/module_interface.py
@@ -6,11 +6,12 @@ import logging
 logger = logging.getLogger(NAME)
 
 from core import api
-import angr
 import time
 
-opts_doc={"timeout": {"type": int, "mangle": True, "default": 600, "description": "Time in seconds for angr before it times out, default is 5 minutes"},
-          "exploration": {"type": str, "mangle": True, "description": "Choose a different exploration technique. Should be from angr.exploration_techniques, such as 'angr.exploration_techniques.dfs'","default":""}
+opts_doc={"timeout": {"type": int, "mangle": True, "default": 120, "description": "Time in seconds for angr before it times out, default is 5 minutes"},
+          "exploration": {"type": str, "mangle": True, "description": "Choose a different exploration technique. Should be from angr.exploration_techniques, such as 'angr.exploration_techniques.dfs'","default":""},
+          "tactics": {"type": str, "mangle": True, "description": "Comma separated list of tactics to use for angr's Z3 solver in a z3.Then() order. Takes precedence over tactic", "default": ""},
+          "tactic": {"type": str, "mangle": True, "description": "Singular tactic to use for angr's Z3 solver. Tactics takes precedence"}
           }
 
 def documentation():
@@ -28,9 +29,8 @@ def k_step_func(simmgr):
         simmgr.move(from_stash="active", to_stash="deadend")
     return simmgr
 
-start_time = None
-timeout = None
-
+start_time = float()
+timeout = float()
 def process(oid, opts):
     """
     This module takes in various parameters as options to
@@ -39,41 +39,78 @@ def process(oid, opts):
     """
     global start_time
     global timeout
+    import angr
+    # this will have to be run twice...
     #set proper timeout
+    my_tactics = []
+    my_tactic = ""
     timeout = opts['timeout']
+    if opts['tactics']:
+        my_tactics = opts['tactics'].split(",")
+        if opts['tactic']:
+            logger.warning("Tactics option overrides tactic, your provided tactic will not be used")
+    elif opts['tactic']:
+        my_tactic = opts['tactic']
     #initialize results output dictionary
-    results = {}
+    results = {'with angr solver': {}}
     #create the angr project for the oid
     data = api.get_field(api.source(oid), oid, "data", {})
     f_name = api.get_field("file_meta", oid, "names").pop()
     f_name = api.tmp_file(f_name, data)
-    data = api.get_field(api.source(oid), oid, "data", {})
     proj = angr.Project(f_name,load_options={"auto_load_libs":False})
     angr_logger = logging.getLogger("angr")
     angr_logger.setLevel(50)
     #make the cfg using cfgfast
-    cfg = proj.analyses.CFGFast()
+    proj.analyses.CFGFast()
     #start and initialize the execution at the entry state of the program
-    entry_state = proj.factory.entry_state()
+    entry_state = proj.factory.entry_state(add_options=angr.options.simplification)
     simgr = proj.factory.simgr(entry_state)
-    supported_options = ["dfs","loops"]
+    #make sure we don't use all the RAM and crash the interpreter
+    #leaving 2GB free of RAM
+    import param_helper
     if opts["exploration"]:
-        match opts["exploration"]:
-            case "dfs":
-                simgr.use_technique(angr.exploration_techniques.DFS())
-            case "loops":
-                simgr.use_technique(angr.exploration_techniques.LoopSeer())
-            case _:
-                logger.warning(f"Unknown exploration option {opts['exploration']}\
-                try from the following: {supported_options}")
-
-    beginning_time = time.time()
+        param_helper.set_simgr_techniques(opts["exploration"],simgr)
     start_time = time.time()
     while simgr.active:
         simgr.step()
     ending_time = time.time()
-
-    results['time taken'] = ending_time-beginning_time
+    results['with angr solver']['states'] = sum([len(simgr.stashes[stash]) for stash in simgr.stashes])
+    results['with angr solver']['time taken'] = ending_time-start_time
+    # i have to reimport angr and stuff each time I call this function, otherwise
+    # there will be some residual leftover stuff in memory that I'd much
+    # rather have cleared
+    # i might need to kill and reload angr after every test.... i know its ugly
+    #refresh angr and things
+    del angr
+    import angr
+    #import backend_z3 so i can use my custom solver
+    from claripy.backends import backend_z3
+    #set the parameter helper's global variables
+    param_helper.my_tactics = my_tactics
+    param_helper.my_tactic = my_tactic
+    #now use my custom solver!
+    backend_z3.BackendZ3.solver = param_helper.my_solver
+    #create the angr project for the oid
+    proj = angr.Project(f_name,load_options={"auto_load_libs":False})
+    angr_logger = logging.getLogger("angr")
+    angr_logger.setLevel(50)
+    #make the cfg using cfgfast
+    proj.analyses.CFGFast()
+    #start and initialize the execution at the entry state of the program
+    #the added option should make angr use Z3 more for simplification
+    entry_state = proj.factory.entry_state(add_options=angr.options.simplification)
+    simgr = proj.factory.simgr(entry_state)
+    if opts["exploration"]:
+        param_helper.set_simgr_techniques(opts["exploration"],simgr)
+    start_time = time.time()
+    while simgr.active:
+        simgr.step()
+    ending_time = time.time()
+    results['with my solver'] = {}
+    results['with my solver']['states'] = sum([len(simgr.stashes[stash]) for stash in simgr.stashes])
+    results['with my solver']['seconds taken'] = ending_time-start_time
+    results['angr strategy'] = opts['exploration'] if opts['exploration'] else "None selected"
+    del angr
     api.store(NAME,oid,results,opts)
     return True
     
---
+++ src/oxide/modules/extractors_dev/angr_parameter_optimization/param_helper.py
@@ -0,0 +1,70 @@
+AUTHOR="KEVAN"
+NAME="angr_parameter_optimization.param_helper"
+"""
+This file hides some of the helper functions used in the angr_parameter_optimization module
+"""
+import logging
+logger = logging.getLogger(NAME)
+my_tactics = []
+my_tactic = ""
+def my_solver(self, timeout=None, max_memory=None):
+    """ 
+    This replaces the backend Z3 solver used by angr
+    """
+    global my_tactics
+    global my_tactic
+    from threading import current_thread, main_thread
+    if not self.reuse_z3_solver or getattr(self._tls, "solver", None) is None:
+        import z3
+        if my_tactics:
+            s = z3.Then(*(z3.Tactic(t,ctx=self._context) for t in my_tactics),ctx=self._context).solver()
+        elif my_tactic:
+            s = z3.Tactic(my_tactic,ctx=self._context).solver()
+        else:
+            s = z3.Solver()  # , logFile="claripy.smt2")
+        if current_thread() != main_thread():
+            s.set(ctrl_c=False)
+        try:
+            import __pypy__
+            __pypy__.add_memory_pressure(1024 * 1024 * 10)
+        except ImportError:
+            _is_pypy = False
+        if self.reuse_z3_solver:
+            # Store the Z3 solver to a thread-local storage if the reuse-solver option is enabled
+            self._tls.solver = s
+    else:
+        # Load the existing Z3 solver for this thread
+        s = self._tls.solver
+        s.reset()
+
+    # Configure timeouts
+    if timeout is not None:
+        if "soft_timeout" in str(s.param_descrs()):
+            s.set("soft_timeout", timeout)
+            s.set("solver2_timeout", timeout)
+        else:
+            s.set("timeout", timeout)
+    if max_memory is not None:
+        s.set("max_memory", max_memory)
+    return s
+
+def set_simgr_techniques(explor, simgr):
+    import angr
+    supported_options = ["dfs","loops","unique"]
+    simgr.use_technique(angr.exploration_techniques.DFS())
+    simgr.use_technique(angr.exploration_techniques.MemoryWatcher(min_memory=2048))
+    if explor:
+        for opt in explor.split(","):
+            match opt:
+                case "dfs":
+                    simgr.use_technique(angr.exploration_techniques.DFS())
+                case "loops":
+                    simgr.use_technique(angr.exploration_techniques.LoopSeer())
+                case "unique":
+                    simgr.use_technique(angr.exploration_techniques.UniqueSearch())
+                case "veritesting":
+                    simgr.use_technique(angr.exploration_techniques.veritesting.Veritesting())
+                case _:
+                    logger.error(f"Unknown exploration option {explor}\
+                    try from the following: {supported_options}")
+                    return False
