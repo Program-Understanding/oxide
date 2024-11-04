040ddd4f1e8921e3c2c1f5a3113fc94b5aa274ee
bakerk98 <baker.kevan98@gmail.com>
2024-11-04 14:15:26 -0600

    Updated module to be ready to run over a large collection of small files

--- src/oxide/modules/extractors_dev/angr_parameter_optimization/module_interface.py
+++ src/oxide/modules/extractors_dev/angr_parameter_optimization/module_interface.py
@@ -1,4 +1,4 @@
-AUTHOR="KEVAN"
+AUTHORS="KEVAN,DYLAN"
 DESC="Pass options to try and analyze effect on angr and Z3"
 NAME="angr_parameter_optimization"
 
@@ -6,6 +6,9 @@ import logging
 import subprocess
 import json
 import os
+import z3
+import statistics
+import scipy
 
 logger = logging.getLogger(NAME)
 
@@ -13,8 +16,7 @@ from core import api
 
 opts_doc={"timeout": {"type": int, "mangle": True, "default": 120, "description": "Time in seconds for angr before it times out, default is 5 minutes"},
           "exploration": {"type": str, "mangle": True, "description": "Choose a different exploration technique. Should be from angr.exploration_techniques, such as 'angr.exploration_techniques.dfs'","default":""},
-          "tactics": {"type": str, "mangle": True, "description": "Comma separated list of tactics to use for angr's Z3 solver in a z3.Then() order. Takes precedence over tactic", "default": ""},
-          "tactic": {"type": str, "mangle": True, "description": "Singular tactic to use for angr's Z3 solver. Tactics takes precedence","default":""},
+          "tactics": {"type": str, "mangle": True, "description": "Comma separated list of tactics to use as a z3.Solver() in claripy", "default": ""},
           "runs": {"type": int, "mangle": True, "description": "How many runs to do of the OID","default": 5}
           }
 
@@ -33,9 +35,22 @@ def process(oid, opts):
     pass to angr or Z3 in order to try and optimize them for
     faster execution speed in angr
     """
-    timeout = opts['timeout']
-    tactic = opts['tactic']
+    #capture options
+    try:
+        timeout = opts['timeout']
+        tactics = opts['tactics'].split(',')
+    except Exception as e:
+        logger.error(f"Unable to wrangle options, error: {e}")
+        return False
+    #check validity of tactics
+    for tactic in tactics:
+        try:
+            z3.Tactic(tactic)
+        except z3.Z3Exception as e:
+            logger.error(f"Unknown tactic: {tactic}, {e}")
+            return False
     num_runs = int(opts['runs'])
+    #create temporary file to run through angr script
     data = api.get_field(api.source(oid), oid, "data", {}) #get file data
     f_name = api.get_field("file_meta", oid, "names").pop()
     f_name = api.tmp_file(f_name, data) #make temp file for angr project
@@ -43,34 +58,69 @@ def process(oid, opts):
     if not os.path.isfile(file_full_path):
         logger.error(f"Scratch file not found: {file_full_path}")
         return False
-    
+    #getting the script name to append to the path
     script_name = "angr_param_optimization.py"
     script_full_path = os.path.join(api.scripts_dir, script_name)
     if not os.path.isfile(script_full_path):
         logger.error(f"Script not found: {script_full_path}")
         return False
-    
-    tactic_cmd = f"python3 {script_full_path} {file_full_path} {timeout} {tactic}"
+    #this has to be run separate times since angr will not let go of RAM
+    #so we'll make as many commands as we have tactics
+    tactic_cmds = []
+    #initialize results dictionary as well
+    results = {}
+    for tactic in tactics:
+        #appending a tuple: command, tactic
+        tactic_cmds.append((f"python3 {script_full_path} {file_full_path} {timeout} {tactic}",tactic))
+        results[tactic] = {'seconds': []}
+    results['with no tactic'] = {'seconds':[]}
     angr_cmd = f"python3 {script_full_path} {file_full_path} {timeout}"
-    
+    #run multiple times to get some valid output and ensure angr isn't doing well as a one-off try
     for run in range(num_runs):
         with open(os.devnull, "w") as null:
             try:
-                tactic_output = subprocess.check_output(tactic_cmd, universal_newlines=True, shell=True, stderr=null)
+                #get the output from angr and with tactics, and translate it to a json dictionary
+                tactic_output = []
+                for tactic_cmd in tactic_cmds:
+                    #using tuple: output, tactic
+                    logger.debug(f'Run {run+1}: command: {tactic_cmd[0]}')
+                    tactic_output.append((subprocess.check_output(tactic_cmd[0], universal_newlines=True, shell=True, stderr=null),tactic_cmd[1]))
+                logger.debug(f'Run {run+1}: command: {angr_cmd}')
                 angr_output = subprocess.check_output(angr_cmd, universal_newlines=True, shell=True, stderr=null)
+                for t_o in tactic_output:
+                    #using tuple: json, tactic
+                    tactic_output = json.loads(t_o[0])
+                    results[t_o[1]][f'run {run+1}'] = tactic_output
+                    results[t_o[1]]['seconds'].append(tactic_output['seconds'])
+                angr_output = json.loads(angr_output)
+                results['with no tactic'][f'run {run+1}'] = angr_output
+                results['with no tactic']['seconds'].append(angr_output['seconds'])
             except subprocess.CalledProcessError as e:
-                logger.warning(e.output)
-
-        if "error" in tactic_output.lower() or "error" in angr_output.lower():
-            return False
-        
-        try:
-            tactics_output = json.loads(tactics_output)
-            angr_output = json.loads(angr_output)
-        except Exception as e:
-            logger.error(f"Error occurred  when loading json: {e}")
-            return False
-        
+                logger.error(f"Error occured: {e}")
+                return False
+            except Exception as e:
+                logger.error(f"Error occurred  when loading json: {e}")
+                return False
+    #get mean, standard deviation, t-test
+    results['with no tactic']['mean seconds'] = statistics.mean(results['with no tactic']['seconds'])
+    results['with no tactic']['std. deviation'] = statistics.stdev(results['with no tactic']['seconds'])
+    for tactic in tactics:
+        results[tactic]['mean seconds'] = statistics.mean(results[tactic]['seconds'])
+        results[tactic]['std. deviation'] = statistics.stdev(results['with no tactic']['seconds'])
+        t_test_p_value = scipy.stats.ttest_ind(results['with no tactic']['seconds'], results[tactic]['seconds'], equal_var=False).pvalue
+        results[tactic][f'p-value of the t-test putting tactic against angr'] = t_test_p_value
+        results[tactic][f'p-value < 0.05'] = float(t_test_p_value) < 0.05
+    #ranking the tactics by mapping each tactic to its average seconds,
+    #but returning only the tactics that are statistically significantly better than
+    #the default solver used by angr
+    mapped_tactics = []
+    for tactic in tactics:
+        if results[tactic][f'p-value < 0.05']:
+            mapped_tactics.append((tactic,results[tactic]['mean seconds']))
+    if mapped_tactics:
+        sorted_tactics = sorted(mapped_tactics,key=lambda mapt: mapt[1])
+        results['statistically significant tactics ranked'] = [(tactic[0],round(tactic[1],4)) for tactic in sorted_tactics]
+    else:
+        results['statistically significant tactics ranked'] = "No statistically significant improvement with any tactic"
+    api.store(NAME,oid,results,opts)
     return True
-
-
