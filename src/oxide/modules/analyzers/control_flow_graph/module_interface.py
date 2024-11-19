7809123aa13f2638029c512e84ee01ad69d7bd28
Camster811 <cls0150@auburn.edu>
2024-11-19 17:46:58 -0600

    update control flow graph data return

--- src/oxide/modules/analyzers/control_flow_graph/module_interface.py
+++ src/oxide/modules/analyzers/control_flow_graph/module_interface.py
@@ -52,7 +52,10 @@ def generate_cfg(oid: str, opts: dict):
 
     cfg = {
         "nodes": [],
-        "edges": []
+        "edges": [],
+        "function_calls": {},
+        "block_calls": {},
+        "functions": {}
     }
     if opts["order_by_node"]:
         cfg["nodes"] = {}
@@ -65,7 +68,7 @@ def generate_cfg(oid: str, opts: dict):
                 cfg["nodes"][block_addr] = block_info["members"]
         else:
             block_node = {
-                "id": block_addr,
+                "block id": block_addr,
                 "instructions": block_info["members"]
             }
             cfg["nodes"].append(block_node)
@@ -92,6 +95,92 @@ def generate_cfg(oid: str, opts: dict):
 
                     cfg["nodes"].append(placeholder_node)
                 node_ids.add(dest)
+
+    # Add function call edges and function call info
+    for func_name, func_info in disasm_output.items():
+        if func_name == 'meta':
+            continue
+        cfg["function_calls"][func_name] = {
+            "called_by": [],
+            "calls": []
+        }
+        for block_addr in func_info['blocks']:
+            if block_addr in original_blocks:
+                for dest in original_blocks[block_addr]['dests']:
+                    if dest in disasm_output:
+                        edge = {
+                            "from": func_name,
+                            "to": disasm_output[dest]['name']
+                        }
+                        cfg["edges"].append(edge)
+                        cfg["function_calls"][func_name]["calls"].append(disasm_output[dest]['name'])
+                        if disasm_output[dest]['name'] not in cfg["function_calls"]:
+                            cfg["function_calls"][disasm_output[dest]['name']] = {
+                                "called_by": [],
+                                "calls": []
+                            }
+                        cfg["function_calls"][disasm_output[dest]['name']]["called_by"].append(func_name)
+
+    # Add block call info and function association
+    for block_addr, block_info in original_blocks.items():
+        cfg["block_calls"][block_addr] = {
+            "called_by": [],
+            "calls": block_info["dests"],
+            "function": None
+        }
+        for dest in block_info["dests"]:
+            if dest not in cfg["block_calls"]:
+                cfg["block_calls"][dest] = {
+                    "called_by": [],
+                    "calls": [],
+                    "function": None
+                }
+            cfg["block_calls"][dest]["called_by"].append(block_addr)
+
+    # Extract functions, basic blocks, and disassembly
+    f_dict = {}
+    names = api.get_field("file_meta", oid, "names")
+    logger.debug("process(%s)", names)
+
+    funs = api.get_field("ghidra_disasm", oid, "functions")
+    if not funs:
+        return False
+    bbs = api.get_field("ghidra_disasm", oid, "original_blocks")
+    insns = api.get_field("disassembly", oid, oid)
+    if insns and "instructions" in insns:
+        insns = insns["instructions"]
+    else:
+        return False
+
+    range = sorted(insns.keys())
+    logger.info("Instruction range: %d - %d", range[0], range[-1])
+
+    extracts = {}
+    for f in funs:
+        if f == 'meta':
+            continue
+        fname = funs[f]['name']
+        extracts[fname] = funs[f]
+        blocks = funs[f]['blocks']
+        extracts[fname]['instructions'] = {}
+        for b in blocks:
+            if b not in bbs:
+                continue
+            for insn_offset, insn_text in bbs[b]['members']:
+                if insn_offset not in insns.keys():
+                    logger.error("Basic Block member not found: %s", insn_offset)
+                    continue
+                extracts[fname]['instructions'][insn_offset] = insn_text
+            # Associate block with function
+            if b in cfg["block_calls"]:
+                cfg["block_calls"][b]["function"] = fname
+        range = sorted(extracts[fname]['instructions'].keys())
+        if range:
+            extracts[fname]["start"] = range[0]
+        else:
+            extracts[fname]["start"] = None
+
+    cfg["functions"] = extracts
 
     return cfg
 
