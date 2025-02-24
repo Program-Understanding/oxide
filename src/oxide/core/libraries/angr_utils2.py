5da3ac9df4fcdb89dab3e142f9b858fa13d29ea3
bakerk98 <baker.kevan98@gmail.com>
2025-02-24 14:33:45 -0600

    Updated angr_lib to improve timed function calls and support asking what registers and ending constraints look like from running a function

--- src/oxide/core/libraries/angr_utils2.py
+++ src/oxide/core/libraries/angr_utils2.py
@@ -28,21 +28,27 @@ def ufr_step_func(simgr):
 class angrTherapy:
     def __init__(self,oid):
         import angr
+        from claripy import backends
         """
         initiate an angr project given an oid
-        """        
+        """
         data=api.get_field(api.source(oid),oid,"data",{})
         f = api.get_field("file_meta",oid,"names").pop()
         f = api.tmp_file(f,data)
         self._proj = angr.Project(f)
+        self._opts = angr.options
+        self._convert = backends.z3.convert
 
-    def timed_underconstrained_function_run(self,function_offset: int,timeout=600)->tuple | bool:
+    def timed_underconstrained_function_run(self,function_offset: int,timeout:int=600)->tuple | bool:
         from time import time
+        #first run the analysis that'll make simprocedures for all other function calls
+        self._proj.analyses.CalleeCleanupFinder()
         #fun_offset is like an int like 4096 or something
         angr_fun_addr = self._proj.loader.min_addr+function_offset
         if angr_fun_addr > self._proj.loader.max_addr:
             return False
-        fun_call_state = self._proj.factory.call_state(angr_fun_addr)
+        #add the option to run the call state w/o following calls
+        fun_call_state = self._proj.factory.call_state(angr_fun_addr,add_options={self._opts.LAZY_SOLVES})        
         simgr = self._proj.factory.simgr(fun_call_state)
         start_time = time()
         try:
@@ -56,18 +62,46 @@ class angrTherapy:
         except Exception:
             return False
 
-    def loop_analyzer(self):
+    def function_constraints(self,function_offset:int, registers:list[str]=[],timeout:int=600)->tuple|bool:
+        """ Given the address of a function, return constraints based upon the function's run-through """
+        constraints = []
+        registers_constraints = []
+        angr_fun_addr = self._proj.loader.min_addr+function_offset
+        if angr_fun_addr > self._proj.loader.max_addr:
+            return False
+        fun_call_state = self._proj.factory.call_state(angr_fun_addr)
+        simgr = self._proj.factory.simgr(fun_call_state)
+        from time import time
+        start_time = time()
         try:
-            analysis = self._proj.analyses.LoopAnalysis()
-        except Exception as e:
-            analysis = dir(self._proj.analyses)
-            return analysis
+            while simgr.active:
+                simgr.step(step_func=ufr_step_func)
+                if time()-start_time > timeout:
+                    #return false if we run out of time
+                    return False
+                if "lowmem" in simgr.stashes and len(simgr.stashes["lowmem"]) > 0:
+                    #return false for now if we run out of memory
+                    return False
+            for state in simgr.deadended:
+                constraints.append(self._convert(state.solver.constraints))
+                for register in registers:
+                    if hasattr(state,register):
+                        registers_constraints.append(self._convert(getattr(state,register)))
+                    else:
+                        logger.warning(f"State doesn't have register {register}")
+            if len(simgr.errored) > 0:
+                logger.warning("There have been states that errored")
+            for state in simgr.errored:
+                constraints.append(self._convert(state.state.solver.constraints))
+            return constraints, registers_constraints
+        except Exception:
+            return False
 
 class angrManagerProxy(BaseProxy):
     _exposed_ = ("timed_underconstrained_function_run","loop_analyzer")
     def timed_underconstrained_function_run(self, function_offset: int,timeout=600):
         return self._callmethod("timed_underconstrained_function_run",(function_offset,timeout))
-    def loop_analyzer(self):
-        return self._callmethod("loop_analyzer")
+    def function_constraints(self,function_offset:int,registers:list[str]=[],timeout:int=600):
+        return self._callmethod("function_constraints")
 
 angrManager.register("angr_project",angrTherapy)
