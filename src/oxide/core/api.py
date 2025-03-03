0e15d3441f336974c48ec4c89b777e32f1f6502b
bakerk98 <baker.kevan98@gmail.com>
2025-03-03 14:51:24 -0600

    Updated api to better support LSP

--- src/oxide/core/api.py
+++ src/oxide/core/api.py
@@ -1,105 +1,125 @@
-"""
-Copyright 2023 National Technology & Engineering Solutions
-of Sandia, LLC (NTESS). Under the terms of Contract DE-NA0003525 with NTESS,
-the U.S. Government retains certain rights in this software.
-
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in
-all copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-THE SOFTWARE.
-"""
-from typing import Any, List, Optional, Tuple, Protocol
+from typing import Any, List, Optional, Tuple, Dict, Set
 # pyright: reportAssignmentType=false
+# pyright: reportUnusedVariable=false
 # These will be wired in oxide.py
 # so any lsp errors here are incorrect
 
-class Retrieve(Protocol):
-    def __call__(self, mod_name: str, oid_list: List[str] | str, opts: dict = {},
+def apply_tags(oid_list: List[str], new_tags: List[str]) -> None:
+    ...
+def collection_names() -> List[str]:
+    ...
+def collection_cids() -> List[str]:
+    ...
+def create_collection(col_name: str, oid_list: List[str], notes: str = "") -> bool:
+    ...
+def delete_collection_by_cid(cid):
+    ...
+def delete_collection_by_name(col_name):
+    ...
+def documentation(mod_name: str) -> Optional[dict]:
+    ...
+def exists(mod_name: str, oid: str, opts={}, dir_override: Optional[str] = None) -> bool:
+    ...
+def expand_oids(oids: str| List[str]) -> List[str]:
+        ...
+def flush_oid(oid: str) -> None:
+    ...
+def flush_module(mod_name: str) -> None:
+    ...
+def get_cid_from_oid_list(oid_list):
+    ...
+def get_cid_from_name(col_name):
+    ...
+def get_colname_from_oid(oid: str) -> Set[str]:
+    ...
+def get_field(mod_name: str, oid: str, field: str,
+              opts: dict = {}, dir_override: Optional[str] = None) -> Optional[Any]:
+    """
+    Given a module name, oid and a field return the value of that field
+
+    Args:
+    mod_name (str): The name of the module
+    oid (str): OID to query
+    field (str): The field to search for from the module's return dict
+    opts (dict): The options to pass the module
+    dir_override(Optional[str])
+    """
+    ...
+def get_names_from_oid(oid: str) -> Set[str]:
+    ...
+def get_oids_with_name(some_name: str) -> Dict[str, str]:
+    ...
+def get_oid_from_data(data: bytes) -> str:
+    ...
+def get_available_modules(category: Optional[str] = 'all') -> List[str]:
+    ...
+def get_orphan_oids() -> List[str]:
+    ...
+def get_tags(oid: str) -> Optional[List[str]]:
+    ...
+def import_file(file_location: str, dir_override: Optional[str] = None) -> Tuple[Optional[str], bool]:
+    ...
+def import_directory(directory: str, dir_override: Optional[str] = None) -> Tuple[List[str], int]:
+    ...    
+def local_available_data(plugin_name: str) -> List[str]:
+    ...
+def local_retrieve_all(plugin_name: str) -> Dict[str, bytes]:
+    ...
+def local_delete_data(plugin_name: str, data_name: str) -> bool:
+    """ Given an oid and the name of a module, remove the data for that
+        combination if it exists.
+    """
+    ...
+def local_delete_function_data(plugin_name: str) -> bool:
+    """ Remove all stored data for a given module.
+    """
+    ...
+def local_exists(plugin_name: str, data_name: str) -> bool:
+    """ Determine whether file exists on local file system
+    """
+    ...
+def local_retrieve(plugin_name: str, data_name: str) -> Optional[Any]:
+        ...
+def retrieve_all(mod_name: str, dir_override: Optional[str] = None) -> dict:
+    ...
+def local_store(plugin_name: str, data_name: str, data: Any) -> bool:
+        ...
+def load_reference(ref_name: str) -> Optional[bytes]:
+    ...
+models_dir = "UNUSED"
+def modules_list(module_type: str = "all", show_private: bool = False):
+    ...
+def retrieve(mod_name: str, oid_list: List[str] | str, opts: dict = {},
              lock: bool = False, dir_override: Optional[str] = None) -> Optional[dict]:
         ...
-
-class LocalRetrieve(Protocol):
-    def __call__(self,plugin_name: str, data_name: str) -> Optional[Any]:
-        ...
-
-class LocalStore(Protocol):
-    def __call__(self,plugin_name: str, data_name: str, data: Any) -> bool:
-        ...
-
-class GetField(Protocol):
-    def __call__(self,mod_name: str, oid: str, field: str, opts: dict = {},
-              dir_override: Optional[str] = None) -> Optional[Any]:
-                 ...
-class ValidOids(Protocol):
-    def __call__(self,l: List[str]) -> Tuple[List[str], List[str]]:
-        ...
-
-class ExpandOids(Protocol):
-    def __call__(self,oids: str| List[str]) -> List[str]:
-        ...
-
-apply_tags                 = None
-collection_names           = None
-collection_cids            = None
-create_collection          = None
-delete_collection_by_cid   = None
-delete_collection_by_name  = None
-documentation              = None
-exists                     = None
-expand_oids                : ExpandOids = None
-flush_oid                  = None
-flush_module               = None
-get_cid_from_oid_list      = None
-get_cid_from_name          = None
-get_colname_from_oid       = None
-get_field                  : GetField = None
-get_names_from_oid         = None
-get_oids_with_name         = None
-get_oid_from_data          = None
-get_available_modules      = None
-get_orphan_oids            = None
-get_tags                   = None
-import_file                = None
-import_directory           = None
-local_available_data       = None
-local_count_records        = None
-local_delete_data          = None
-local_delete_function_data = None
-local_exists               = None
-local_retrieve             : LocalRetrieve = None
-local_retrieve_all         = None
-local_store                : LocalStore = None
-load_reference             = None
-models_dir                 = None
-modules_list               = None
-retrieve                   : Retrieve = None
-retrieve_all_keys          = None
-scratch_dir                = None
-libraries_dir              = None
-ghidra_path                = None
-ghidra_path2               = None
-ghidra_path3               = None
-ghidra_path4               = None
-ghidra_path5               = None
-ghidra_project             = None
-pharos_image               = None
-source                     = None
-store                      = None
-process                    = None
-tag_filter                 = None
-valid_oids                 : ValidOids= None
-tmp_file                   = None
-scripts_dir                = None
+def retrieve_all_keys(mod_name: str) -> Optional[List[str]]:
+    ...
+scratch_dir = None
+libraries_dir = None
+ghidra_path = ""
+ghidra_path2 = ""
+ghidra_path3 = ""
+ghidra_path4 = ""
+ghidra_path5 = ""
+ghidra_project = ""
+pharos_image = ""
+def source(oid: str, dir_override: Optional[str] = None) -> Optional[str]:
+    ...
+def store(mod_name: str, oid: str, data: Dict[str|int,Any], opts: dict = {}, block: bool = True,
+          dir_override: Optional[str] = None)->bool:
+    ...
+def process(mod_name: str, oid_list: List[str], opts: dict = {},
+            force: bool = False, dir_override: Optional[str] = None) -> bool:
+    """ Calls a module over an oid_list without returning results.
+    """
+    ...
+def tag_filter(oid_list: List[str], tag: str, value: str = "<empty>") -> Optional[List[str]]:
+    ...
+def valid_oids(l: List[str]) -> Tuple[List[str], List[str]]:
+    ...
+def tmp_file(name: str, data: bytes, empty: bool = False, override: bool = False) -> Optional[str]:
+    """ Given a file name and data uses the Python tempfile package to write the file to
+        the scratch directory.
+    """
+    ...
+scripts_dir = ""
