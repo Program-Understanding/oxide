7e5e364cc75d412d74c691786d68bbd3a05a521d
dennisgbrown <dennis.g.brown@gmail.com>
2025-04-14 10:50:39 -0500

    added function disassembly. standardized tool names and comments.

--- mcp_server.py
+++ mcp_server.py
@@ -56,23 +56,6 @@ from oxide.core import oxide as oxide
 
 mcp = FastMCP("oxide")
 
-# Commented out because it didn't look finished yet (e.g., no input args, "test")
-# @mcp.tool()
-# async def get_oids() -> dict[Any,Any] | Literal[False]:
-#     """
-#     Given a name, be able to get an OID back which corresponds to the name
-#
-#     Args:
-#     No args
-#     """
-#     result = oxide.get_oids_with_name("test")
-#     if result:
-#         logger.info("Got oids with names")
-#         return result
-#     else:
-#         logger.error(f"Couldn't get oids and names")
-#         return False
-
 @mcp.tool()
 async def get_function_offset(oid: str, function_names: list[str]) -> dict[Any, Any] | Literal[False]:
     """
@@ -160,33 +143,34 @@ async def get_function_name(oid: str, function_offsets: list[int]) -> dict[Any, 
 
 
 @mcp.tool()
-async def function_summary(oid: str) -> dict[Any,Any] | Literal[False]:
+async def get_function_summaries(oid: str) -> dict[Any,Any] | Literal[False]:
     """
-    Retrieve summaries of functions for a binary file.
+    Retrieve summaries of all functions in a binary file.
     
-    Returns: JSON with summary information about each function in the binary file 
-    in a dictionary format. The dictionary key is function name and the value is
-    another dictionary containing: offset, signature, number of instructions, complexity,
-    operands, and parameters.
-    Example:
-    {
-        \"_init\": {
-            \"offset\": 4096, 
-            \"signature\": \"int __stdcall _init(EVP_PKEY_CTX * ctx)\", 
-            \"num_insns\": 8, 
-            \"complexity\": 7, 
-            \"complexity_desc\": \"simple\", 
-            \"operands\": {
-                \"imm\": 3, 
-                \"reg\": 6, 
-                \"mem\": 1
-            }, 
-            \"params\": [\"EVP_PKEY_CTX *\"]
-        }
-    }
+    Inputs:
+        oid: the object id of the binary we are interested in
 
-    Args:
-    oid: the object id of the binary we are interested in
+    Returns: 
+        JSON with summary information about each function in the binary file 
+        in a dictionary format. The dictionary key is function name and the value is
+        another dictionary containing: offset, signature, number of instructions, complexity,
+        operands, and parameters.
+        Example:
+        {
+            \"_init\": {
+                \"offset\": 4096, 
+                \"signature\": \"int __stdcall _init(EVP_PKEY_CTX * ctx)\", 
+                \"num_insns\": 8, 
+                \"complexity\": 7, 
+                \"complexity_desc\": \"simple\", 
+                \"operands\": {
+                    \"imm\": 3, 
+                    \"reg\": 6, 
+                    \"mem\": 1
+                }, 
+                \"params\": [\"EVP_PKEY_CTX *\"]
+            }
+        }
     """
     result = oxide.get_field("function_summary", [oid], oid)
     if result:
@@ -197,9 +181,9 @@ async def function_summary(oid: str) -> dict[Any,Any] | Literal[False]:
         return False
     
 @mcp.tool()
-async def control_flow_graph(oid: str, function_name: str = None, function_offset:int = None) -> dict[Any, Any] | Literal[False]:
+async def get_control_flow_graph(oid: str, function_name: str = None, function_offset:int = None) -> dict[Any, Any] | Literal[False]:
     """
-    Retrieve the Control Flow Graph (CFG) for functions in a binary, optionally filtering by function name or function offset.
+    Get the Control Flow Graph (CFG) for functions in a binary, optionally filtering by function name or function offset.
 
     **Inputs:**
       - oid (str): The object ID of the binary to be analyzed.
@@ -251,30 +235,31 @@ async def control_flow_graph(oid: str, function_name: str = None, function_offse
         return False
 
 @mcp.tool()
-async def call_graph(oid: str) -> dict[Any,Any] | Literal[False]:
+async def get_call_graph(oid: str) -> dict[Any,Any] | Literal[False]:
     """
-    Retrieve a call graph for a binary file.
+    Get a call graph for a binary file to know how functions call each other.
     
-    Returns: A JSON dictionary with two entries: nodes and links. Nodes is a list of dictionaries
-    providing the id for each nodes, which are function offsets and not function names. 
-    Links is a list of dictionaries providing the offsets of the source and target functions
-    for each call. 
-    Example:
-    {
-        "nodes": [
-            {"id": 4096}, 
-            {"id": 20520}, 
-            {"id": 4288},
-            {"id": 20512} 
-        ], 
-        "links": [
-            {"source": 4096, "target": 20520}, 
-            {"source": 4288, "target": 20512}
-        ]
-    }
+    Inputs:
+        oid: the object id of the binary we are interested in
 
-    Args:
-    oid: the object id of the binary we are interested in
+    Returns: 
+        A JSON dictionary with two entries: nodes and links. Nodes is a list of dictionaries
+        providing the id for each nodes, which are function offsets and not function names. 
+        Links is a list of dictionaries providing the offsets of the source and target functions
+        for each call. 
+        Example:
+        {
+            "nodes": [
+                {"id": 4096}, 
+                {"id": 20520}, 
+                {"id": 4288},
+                {"id": 20512} 
+            ], 
+            "links": [
+                {"source": 4096, "target": 20520}, 
+                {"source": 4288, "target": 20512}
+            ]
+        }
     """
     result = oxide.retrieve("call_graph", [oid])
     if result:
@@ -290,9 +275,9 @@ async def call_graph(oid: str) -> dict[Any,Any] | Literal[False]:
         return False
 
 @mcp.tool()
-async def call_mapping(oid: str, function_offset: int = None) -> dict[Any, Any] | Literal[False]:
+async def get_call_mapping(oid: str, function_offset: int = None) -> dict[Any, Any] | Literal[False]:
     """
-    Retrieve a mapping of call relationships for functions in a binary, with an option to filter by a specific function offset.
+    Get a mapping of call relationships for functions in a binary, with an option to filter by a specific function offset.
 
     **Inputs:**
       - oid (str): The object ID of the binary to be analyzed.
@@ -336,7 +321,7 @@ async def call_mapping(oid: str, function_offset: int = None) -> dict[Any, Any] 
         return False
 
 @mcp.tool()
-async def file_stats(oid: str) -> dict[Any,Any] | Literal[False]:
+async def get_file_stats(oid: str) -> dict[Any,Any] | Literal[False]:
     """
     Get general file stats for a binary file
     
@@ -363,7 +348,7 @@ async def file_stats(oid: str) -> dict[Any,Any] | Literal[False]:
         return False
 
 @mcp.tool()
-async def strings(oid: str) -> dict[Any,Any] | Literal[False]:
+async def get_strings(oid: str) -> dict[Any,Any] | Literal[False]:
     """
     Get ASCII strings contained in a binary file
 
@@ -376,8 +361,8 @@ async def strings(oid: str) -> dict[Any,Any] | Literal[False]:
         "1219": "__isoc99_scanf"
     }
 
-    Args:
-    oid: the object id of the binary we are interested in
+    Inputs:
+        oid: the object id of the binary we are interested in
     """
     result = oxide.retrieve("strings", [oid])
     if result:
@@ -389,14 +374,19 @@ async def strings(oid: str) -> dict[Any,Any] | Literal[False]:
         return False
 
 @mcp.tool()
-async def disasm(oid: str) -> dict[Any,Any] | Literal[False]:
+async def get_disassembly_for_file(oid: str) -> dict[Any,Any] | Literal[False]:
     """
-    Retrieve a dictionary of disassembly for an OID.
-    Keys in the dictionary which refer to addresses in the binary are typically file offset and not vaddr
-    Values in the disassembly may use vaddr that isn't translated to file offset.
+    Get disassembly of an entire binary file. WARNING: The size of the result is likely to
+    exceed the context window of most LLMs. Use get_details_and_disassembly_for_function to extract
+    individual functions whenever possible.
 
-    Args:
-    oid: the object id of the binary we are interested in
+    Inputs:
+        oid: the object id of the binary we are interested in
+
+    Returns:
+        A JSON dictionary of disassembly for an OID.
+        Keys in the dictionary which refer to addresses in the binary are typically file offset and not vaddr
+        Values in the disassembly may use vaddr that isn't translated to file offset.
     """
     result = oxide.retrieve("disassembly", [oid])
     if result:
@@ -406,34 +396,83 @@ async def disasm(oid: str) -> dict[Any,Any] | Literal[False]:
         logger.error("Failed to retrieve disassembly")
         return False
 
-# @mcp.tool()
-# async def disassemble_function(oid:str, function_name:str) -> str | Literal[False]:
-#     """
-#     Return the disassembly for a specified function as a string. 
+@mcp.tool()
+async def get_details_and_disassembly_for_function(oid:str, function_name:str) -> str | Literal[False]:
+    """
+    Get the details and disassembly for a specified function. 
 
-#     Args:
-#         oid: the ID of the binary file containing the desired function
-#         function_name: the name of the desired function
-#     """
-#     result = oxide.retrieve("disassembly", [oid])
-#     if result:
-#         logger.info("Retrieved disassembly successfully!")
-#         return_str = ''
-#         return return_str
+    Inputs:
+        oid: the ID of the binary file containing the desired function
+        function_name: the name of the desired function
 
-#     else:
-#         logger.error("Failed to retrieve ghidra decompilation mapping")
-#         return False
+    Returns:
+        A JSON dictionary containing: 
+        - name: function name
+        - vaddr: virtual address where the function starts
+        - blocks: list of offsets of basic blocks in the function
+        - params: list of input parameters to the function
+        - retType: return type of the function
+        - signature: signature of the function
+        - returning: does the function return
+        - instructions: dictionary of the disassembly of the function. This dictionary maps
+          the offset of the instruction to the text of the instruction.
+        - start: the offset of the first instruction of the function.
+
+    Example:
+    {
+        "name":"_init",
+        "vaddr":"00101000",
+        "blocks":[
+            4096,
+            4116,
+            4118
+        ],
+        "params":[
+            "EVP_PKEY_CTX *"
+        ],
+        "retType":"[int <RETURN>@EAX:4]",
+        "signature":"int __stdcall _init(EVP_PKEY_CTX * ctx)",
+        "returning":"true",
+        "instructions":{
+            "4096":"endbr64",
+            "4100":"sub  rsp,0x8",
+            "4104":"mov  rax,qword ptr [0x00103fe8]",
+            "4111":"test  rax,rax",
+            "4114":"jz  0x00101016",
+            "4116":"call  rax",
+            "4118":"add  rsp,0x8",
+            "4122":"ret"
+        },
+        "start":4096
+    }
+    """
+    result = oxide.retrieve("function_extract", [oid])
+    if result:
+        logger.info("Retrieved function_extract successfully!")
+        if (function_name not in result.keys()):
+            return False        
+        # print(result[function_name], file=sys.stderr)
+        return result[function_name]
+    else:
+        logger.error("Failed to retrieve function_extract")
+        return False
 
 @mcp.tool()
-async def ghidra_decmap(oid:str, org_by_func: bool) -> dict[Any,Any] | Literal[False]:
+async def get_decompilation_mapping_for_file(oid:str, org_by_func: bool) -> dict[Any,Any] | Literal[False]:
     """
     Get ghidra decompilation mapping, optionally organized by function.
-    Keys in the dictionary which refer to addresses in the binary are typically file offset and not vaddr
+    WARNING: The size of the result is likely to
+    exceed the context window of most LLMs. Use get_source_for_function to extract
+    individual functions whenever possible.
 
-    Args:
-    oid: the object id of the binary we are interested in
-    org_by_func: boolean of whether or not to organize the results by function
+    Inputs:
+        oid: the object id of the binary we are interested in
+        org_by_func: boolean of whether or not to organize the results by function
+
+    Returns: 
+        A JSON dictionary mapping offset addresses to decompiled code lines.
+        Keys in the dictionary which refer to addresses in the binary are typically 
+        file offset and not vaddr
     """
     result = oxide.retrieve("ghidra_decmap", [oid], {"org_by_func": org_by_func})
     if result:
@@ -444,13 +483,16 @@ async def ghidra_decmap(oid:str, org_by_func: bool) -> dict[Any,Any] | Literal[F
         return False
 
 @mcp.tool()
-async def decompile_function(oid:str, function_name:str) -> str | Literal[False]:
+async def get_source_for_function(oid:str, function_name:str) -> str | Literal[False]:
     """
-    Returns the decompilation for a specified function as a string. 
+    Get the C source code (reconstructed through decompilation) for a specified function as a string. 
 
-    Args:
+    Inputs:
         oid: the ID of the binary file containing the desired function
         function_name: the name of the desired function
+
+    Returns: 
+        a string containing the source code for the function
     """
     result = oxide.retrieve("ghidra_decmap", [oid], {"org_by_func": True})
     if result:
